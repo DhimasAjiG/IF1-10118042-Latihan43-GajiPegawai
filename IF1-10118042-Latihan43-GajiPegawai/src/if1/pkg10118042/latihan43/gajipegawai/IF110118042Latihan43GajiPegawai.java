52f693a8d47ff086b61432b7747c6d65afa01a64
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-11-07 19:27:25 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan43-GajiPegawai/src/if1/pkg10118042/latihan43/gajipegawai/IF110118042Latihan43GajiPegawai.java
@@ -0,0 +1,18 @@
+
+package if1.pkg10118042.latihan43.gajipegawai;
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini berfungsi untuk Mengecek Gaji Pegawai
+ */
+public class IF110118042Latihan43GajiPegawai {
+
+    public static void main(String[] args) {
+       GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
+    
